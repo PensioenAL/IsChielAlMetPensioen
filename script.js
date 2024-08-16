5db98fbc6c443d99756c63c2247bd182fafbb5c1
FL-Julien <Julien@florisoft.nl>
2024-08-16 12:13:36 +0200

    final

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ document.addEventListener("DOMContentLoaded", function () {
             '1.mp3', '2.mp3', '3.mp3', '4.mp3', '5.mp3',
             '6.mp3', '7.mp3', '8.mp3', '9.mp3', '10.mp3',
             '11.mp3', '12.mp3', '13.mp3', '14.mp3', '15.mp3',
-            '16.mp3', '17.mp3', '18.mp3'
+            '16.mp3', '17.mp3', '18.mp3','19.mp3','20.mp3','21.mp3'
         ];
 
         // Randomly select one of the sound files
